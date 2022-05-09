243b2478f14b3ec64c276658ccc170cfb277b136
IIIEA <egor.solo2000@bk.ru>
2022-05-09 11:50:36 +0300

    Add camera controller

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -5,15 +5,21 @@ public class CameraFollower : MonoBehaviour
     [SerializeField] private Transform _currentTarget;
     [SerializeField] private Transform _lookTarget;
     [SerializeField] private float _smoothSpeed = 0.125f;
+    [SerializeField] private Vector3 _offset;
+    [SerializeField] private float _downBorder;
 
-    private Vector3 _offset;
     private Vector3 _targetAfterEndLevel = new Vector3(10, 0, -24);
 
     private void FixedUpdate()
     {
         Vector3 positionToGo = _currentTarget.position + _offset;
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, positionToGo, _smoothSpeed * Time.deltaTime);
+
+        if(positionToGo.y <= _downBorder)
+        {
+            positionToGo = new Vector3(positionToGo.x, _downBorder, positionToGo.z);
+        }
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, positionToGo, _smoothSpeed);
         transform.position = smoothPosition;
-        transform.LookAt(_lookTarget.position);
     }
 }
